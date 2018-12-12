cdf846f7b76fec2e35a4e6a5042ecda577ee543c
Stuart McDonald <stuart@gladius.io>
2018-12-11 20:47:41 -0500

    Add google logging to unit tests

--- src/tests/MasternodeTests.cpp
+++ src/tests/MasternodeTests.cpp
@@ -1,10 +1,13 @@
 #include <gtest/gtest.h>
+#include <glog/logging.h>
 
 TEST (MasternodeTests, Foo) {
-    EXPECT_EQ (1, 1);
+  EXPECT_EQ (1, 1);
 }
 
 int main(int argc, char **argv) {
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
