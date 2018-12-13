f79d12321d458e54831b224ce2c8e8d2d5450937
Stuart McDonald <stuart@gladius.io>
2018-12-12 20:49:54 -0500

    Update unit test libs

--- src/tests/MasternodeTests.cpp
+++ src/tests/MasternodeTests.cpp
@@ -1,8 +1,70 @@
 #include <gtest/gtest.h>
 #include <glog/logging.h>
+#include <boost/thread.hpp>
+
+#include <proxygen/httpserver/HTTPServer.h>
+
+#include "ProxyHandlerFactory.h"
+
+using namespace folly;
+using namespace proxygen;
+using namespace masternode;
+
+class ServerThread {
+ private:
+  boost::barrier barrier_{2};
+  std::thread t_;
+  HTTPServer* server_{nullptr};
+
+ public:
+
+  explicit ServerThread(HTTPServer* server) : server_(server) {}
+  ~ServerThread() {
+    if (server_) {
+      server_->stop();
+    }
+    t_.join();
+  }
+
+  bool start() {
+    bool throws = false;
+    t_ = std::thread([&]() {
+      server_->start([&]() { barrier_.wait(); },
+                     [&](std::exception_ptr /*ex*/) {
+                       throws = true;
+                       server_ = nullptr;
+                       barrier_.wait();
+                     });
+    });
+    barrier_.wait();
+    return !throws;
+  }
+};
 
 TEST (MasternodeTests, Foo) {
   EXPECT_EQ (1, 1);
+}
+
+TEST (ProxyHandlerFactory, TestStartsWithServer) {
+
+
+  std::vector<HTTPServer::IPConfig> IPs = {
+        {folly::SocketAddress("0.0.0.0", 8080, true),
+        HTTPServer::Protocol::HTTP}};
+
+  HTTPServerOptions options;
+  options.threads = 1;
+  options.idleTimeout = std::chrono::milliseconds(60000);
+  options.shutdownOn = {SIGINT, SIGTERM};
+  options.enableContentCompression = false;
+  options.handlerFactories =
+      RequestHandlerChain()
+          .addThen<ProxyHandlerFactory>()
+          .build();
+  auto server = std::make_unique<HTTPServer>(std::move(options));
+  auto st = std::make_unique<ServerThread>(server.get());
+  server->bind(IPs);
+  EXPECT_TRUE(st->start());
 }
 
 int main(int argc, char **argv) {
