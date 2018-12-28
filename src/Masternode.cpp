b4205bda95afa37f120c2b1ba5b64a91aec4f372
Stuart McDonald <stuart@gladius.io>
2018-12-27 21:50:14 -0500

    Make SSL optional

--- src/Masternode.cpp
+++ src/Masternode.cpp
@@ -12,54 +12,61 @@ DEFINE_string(ip, "0.0.0.0", "IP/Hostname to bind to");
 DEFINE_int32(port, 80, "Port to listen for HTTP requests on");
 DEFINE_string(origin_host, "0.0.0.0", "IP/Hostname of protected origin");
 DEFINE_int32(origin_port, 80, "Port to contact the origin server on");
-DEFINE_string(protected_host, "localhost", "Hostname of protected host"); // i.e. blog.gladius.io
+DEFINE_string(protected_host, 
+    "localhost", "Hostname of protected host"); // i.e. blog.gladius.io
 DEFINE_string(cert_path, "", "Path to SSL certificate");
 DEFINE_string(key_path, "", "Path to SSL private key");
 DEFINE_string(cache_dir, "", "Path to directory to write cached files to");
+DEFINE_string(gateway_address, "http://0.0.0.0:3000", 
+    "Address to the masternode's Gladius network gateway");
 
 int main(int argc, char *argv[]) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  google::InitGoogleLogging(argv[0]);
-  google::InstallFailureSignalHandler();
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
+    google::InstallFailureSignalHandler();
 
-  size_t threads = sysconf(_SC_NPROCESSORS_ONLN);
-  LOG(INFO) << "Configuring server to use " << threads << " I/O threads...\n";
+    size_t threads = sysconf(_SC_NPROCESSORS_ONLN);
+    LOG(INFO) << "Configuring server to use " << threads << " I/O threads...\n";
 
-  std::vector<HTTPServer::IPConfig> IPs = {
-      {folly::SocketAddress(FLAGS_ip, FLAGS_port, true),
-       HTTPServer::Protocol::HTTP},
-       {folly::SocketAddress(FLAGS_ip, 443, true),
+    std::vector<HTTPServer::IPConfig> IPs = {
+        {folly::SocketAddress(FLAGS_ip, FLAGS_port, true),
         HTTPServer::Protocol::HTTP}};
-  LOG(INFO) << "Binding to " << FLAGS_ip << ":" << FLAGS_port << "\n";
+    LOG(INFO) << "Binding to " << FLAGS_ip << ":" << FLAGS_port << "\n";
 
-  // Enable SSL
-  wangle::SSLContextConfig sslCfg;
-  sslCfg.isDefault = true;
-  sslCfg.setCertificate(FLAGS_cert_path, FLAGS_key_path, "");
-  IPs[1].sslConfigs.push_back(sslCfg);
+    if (!(FLAGS_cert_path.empty() || FLAGS_key_path.empty())) {
+        // Enable SSL
+        HTTPServer::IPConfig sslIP = 
+            {folly::SocketAddress(FLAGS_ip, 443, true),
+            HTTPServer::Protocol::HTTP};
+        wangle::SSLContextConfig sslCfg;
+        sslCfg.isDefault = true;
+        sslCfg.setCertificate(FLAGS_cert_path, FLAGS_key_path, "");
+        sslIP.sslConfigs.push_back(sslCfg);
+        IPs.push_back(sslIP);
+    }
+    
+    auto config = std::make_shared<MasternodeConfig>();
+    config->origin_host = FLAGS_origin_host;
+    config->origin_port = FLAGS_origin_port;
+    config->protected_host = FLAGS_protected_host;
+    config->IPs = IPs;
+    config->cache_directory = FLAGS_cache_dir;
+    config->options.threads = threads;
+    config->options.idleTimeout = std::chrono::milliseconds(60000);
+    config->options.shutdownOn = {SIGINT, SIGTERM};
+    config->options.enableContentCompression = false;
+    config->options.handlerFactories =
+        RequestHandlerChain()
+            .addThen<ProxyHandlerFactory>(config)
+            .build();
 
-  auto config = std::make_shared<MasternodeConfig>();
-  config->origin_host = FLAGS_origin_host;
-  config->origin_port = FLAGS_origin_port;
-  config->protected_host = FLAGS_protected_host;
-  config->IPs = IPs;
-  config->cache_directory = FLAGS_cache_dir;
-  config->options.threads = threads;
-  config->options.idleTimeout = std::chrono::milliseconds(60000);
-  config->options.shutdownOn = {SIGINT, SIGTERM};
-  config->options.enableContentCompression = false;
-  config->options.handlerFactories =
-      RequestHandlerChain()
-          .addThen<ProxyHandlerFactory>(config)
-          .build();
-  
-  
-  Masternode master(config);
+    Masternode master(config);
 
-  std::thread t([&]() { master.start(); });
-  t.join();
+    // Start listening for incoming requests
+    std::thread t([&]() { master.start(); });
+    t.join();
 
-  LOG(INFO) << "Process exiting now\n";
+    LOG(INFO) << "Process exiting now\n";
 
-  return 0;
+    return 0;
 }
