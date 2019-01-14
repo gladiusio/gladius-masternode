75ec7933b670dde5897f69c85f8aa8ad94c375d1
Stuart McDonald <stuart@gladius.io>
2019-01-14 12:34:51 -0500

    Separate masternode implementation

---
+++ src/Masternode.cpp
@@ -0,0 +1,32 @@
+#include "Masternode.h"
+
+using namespace masternode;
+
+Masternode::Masternode(std::shared_ptr<MasternodeConfig> config) {
+    config_ = config;
+    state_ = std::make_shared<NetworkState>(config_);
+    cache_ = std::make_shared<ContentCache>(256, config_->cache_directory);
+    config_->options.handlerFactories = proxygen::RequestHandlerChain()
+        .addThen<ProxyHandlerFactory>(config_, state_, cache_)
+        .build();
+
+    server_ = std::make_unique<proxygen::HTTPServer>(
+        std::move(config_->options));
+}
+
+void Masternode::start(std::function<void()> onSuccess,
+    std::function<void(std::exception_ptr)> onError) {
+    if (state_) {
+        state_->beginPollingGateway();
+    }
+    server_->bind(config_->IPs);
+    server_->start(onSuccess, onError);
+}
+
+void Masternode::stop() {
+    if (server_.get()) {
+        server_->stop();
+    }
+}
+
+std::shared_ptr<NetworkState> Masternode::getNetworkState() { return state_; }
--- src/Masternode.h
+++ src/Masternode.h
@@ -22,33 +22,11 @@ namespace masternode {
             // Stores cached web content
             std::shared_ptr<ContentCache> cache_{nullptr};
         public:
-            Masternode(std::shared_ptr<MasternodeConfig> config) {
-                config_ = config;
-                state_ = std::make_shared<NetworkState>(config_);
-                cache_ = std::make_shared<ContentCache>(256, config_->cache_directory);
-                config_->options.handlerFactories = proxygen::RequestHandlerChain()
-                    .addThen<ProxyHandlerFactory>(config_, state_, cache_)
-                    .build();
-
-                server_ = std::make_unique<proxygen::HTTPServer>(
-                    std::move(config_->options));
-            };
-            
+            Masternode(std::shared_ptr<MasternodeConfig>);
             void start(std::function<void()> onSuccess = nullptr,
-                std::function<void(std::exception_ptr)> onError = nullptr) {
-                if (state_) {
-                    state_->beginPollingGateway();
-                }
-                server_->bind(config_->IPs);
-                server_->start(onSuccess, onError);
-            }
+                std::function<void(std::exception_ptr)> onError = nullptr);
+            void stop();
 
-            void stop() {
-                if (server_.get()) {
-                    server_->stop();
-                }
-            }
-
-            std::shared_ptr<NetworkState> getNetworkState() { return state_; }
+            std::shared_ptr<NetworkState> getNetworkState();
     };
 }
